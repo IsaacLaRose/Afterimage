bf34208e9b3d1195384887ec75bf12ff3582e9e8
IsaacLaRose <ihlarose4444@gmail.com>
2026-06-25 15:03:54 -0400

    attempting to match gif length

--- remap.py
+++ remap.py
@@ -29,6 +29,6 @@ def remap_pixels(substance,template, mode):
     if mode == "band":
         frames = anim.band(flat, template, substance, substance_sort, template_sort)
         newarr = np.array(frames[-1])  # grab before padding
-        frames = [Image.fromarray(substance)] * 5 + frames + [frames[-1]] * 5
+        frames = [Image.fromarray(substance)] * 6 + frames + [frames[-1]] * 6
 
     return newarr, frames
